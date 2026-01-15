5d8746095b539013e91ac5f6865428485ccb3bad
userext122021 <95638049+userext122021@users.noreply.github.com>
2026-01-15 14:12:45 +0300

    Update test_msg1.c

--- tests/test1/test_msg1.c
+++ tests/test1/test_msg1.c
@@ -8,7 +8,15 @@ void msg1(int val) { printf("msg1: %d\n", val); }
 void msg2(int val) { printf("msg1: %d\n", val); }
 
 
-int main() {
 
+int main() {
+vx_string_set str;
+    vx_string_set_init(&str);
+    vx_string_set_add(&str,"msg1");
+    vx_string_set_add(&str,"msg2");
+
+    printf(" msg1:%d msg2:%d size:%d\n",vx_string_set_get(&str,"msg1",vx_string_set_get(&str,"msg2",);
+    
+    vx_string_set_uninit(&str);
     return 0;
 }
