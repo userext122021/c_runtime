88c3d7deab0504571394106e1f1702a6a46b2c81
userext122021 <95638049+userext122021@users.noreply.github.com>
2026-01-15 10:21:06 +0300

    Update vx_std.h

--- tests/vx_std.h
+++ tests/vx_std.h
@@ -2,14 +2,13 @@
 #define VX_STD_H
 
 typedef uint8_t vx_byte;
-typedef int vx_id;
-typedef int vx_size;
+typedef unsigned int vx_id;
 
 typedef struct
 {
-vx_size size;
-vx_size item_size;
-vx_size alloc_size; //amount of items to alloc 
+size_t size;
+size_t item_size;
+size_t alloc_size; //amount of items to alloc 
 vx_byte *data;
 }vx_array;
 
